b28b8e095d6afaa197330e96bf080f2c15b05011
WorkComputer_WangJ <jie581825@yeah.net>
2018-12-28 14:09:45 +0800

    Initial commit

---
+++ app/src/main/java/cn/wangj/calcpro/CreditCashActivity.java
@@ -0,0 +1,13 @@
+package cn.wangj.calcpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreditCashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_cash);
+    }
+}
