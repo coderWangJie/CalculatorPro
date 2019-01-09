c1784f4ca0e2f1644c6828fd957abdaf3a17eb50
WorkComputer_WangJ <jie581825@yeah.net>
2019-01-09 10:56:31 +0800

    信用卡套现手续费主体功能完成

--- app/src/main/java/cn/wangj/calcpro/business/credit/CreditCashActivity.java
+++ app/src/main/java/cn/wangj/calcpro/business/credit/CreditCashActivity.java
@@ -2,14 +2,15 @@ package cn.wangj.calcpro.business.credit;
 
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -18,44 +19,36 @@ import cn.wangj.calcpro.R;
 
 public class CreditCashActivity extends BaseActivity implements View.OnClickListener {
 
-    /**
-     * 套现金额
-     */
+    /** 套现金额 */
     @BindView(R.id.et_amount)
     EditText etAmount;
 
-    /**
-     * 套现日期
-     */
+    /** 套现日期 */
     @BindView(R.id.tv_lieDay)
     TextView tvLieDay;
 
-    /**
-     * 套现手续费，单位%
-     */
+    /** 套现手续费，单位% */
     @BindView(R.id.et_fee)
     EditText etFee;
 
-    /**
-     * 信用卡账单日
-     */
+    /** 信用卡账单日 */
     @BindView(R.id.tv_billDay)
     TextView tvBillDay;
 
-    /**
-     * 最后还款日
-     */
+    /** 最后还款日 */
     @BindView(R.id.tv_deadLine)
     TextView tvDeadLine;
 
-    /**
-     * “从卡包中选择”按钮
-     */
-    @BindView(R.id.btn_creditChoose)
-    Button btnChooseCard;
+    @BindView(R.id.tv_tips)
+    TextView tvTips;
+
+//    /** “从卡包中选择”按钮 */
+//    @BindView(R.id.btn_creditChoose)
+//    Button btnChooseCard;
 
     private String[] temp;
     private int lieYear, lieMonth, lieDay; // 记录选择的刷卡日期
+    private int billDay, deadLine; // 账单日、最后还款日
 
     @Override
     protected int setContentResID() {
@@ -64,12 +57,11 @@ public class CreditCashActivity extends BaseActivity implements View.OnClickList
 
     @Override
     protected void runOnCreate() {
-
+        temp = getResources().getStringArray(R.array.billDays);
     }
 
     @Override
     protected void runOnResume() {
-        temp = getResources().getStringArray(R.array.billDays);
     }
 
     @OnClick({ R.id.tv_lieDay, R.id.tv_billDay, R.id.tv_deadLine, R.id.btn_calculate})
@@ -77,6 +69,7 @@ public class CreditCashActivity extends BaseActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.tv_lieDay:
+                Calendar today = Calendar.getInstance();
                 DatePickerDialog dialog = new DatePickerDialog(this,
                         android.app.AlertDialog.THEME_DEVICE_DEFAULT_LIGHT,
                         new DatePickerDialog.OnDateSetListener() {
@@ -87,44 +80,60 @@ public class CreditCashActivity extends BaseActivity implements View.OnClickList
                                 lieDay = dayOfMonth;
                                 tvLieDay.setText(lieYear + "-" + (lieMonth + 1) + "-" + lieDay);
                             }
-                        }, 2019, 0, 3);
+                        }, today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH));
                 dialog.show();
                 break;
 
             case R.id.tv_billDay:
-                AlertDialog listDialog = new AlertDialog.Builder(this)
-                        .setTitle("请选择每月最后还款日")
+                AlertDialog billDaySelector = new AlertDialog.Builder(this)
+                        .setTitle("请选择每月账单日")
                         .setItems(temp, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(CreditCashActivity.this,
-                                        "你点击了" + temp[which],
-                                        Toast.LENGTH_SHORT).show();
+                                billDay = which + 1;
+                                tvBillDay.setText("每月" + billDay + "日");
                             }
                         }).create();
 
 
-                listDialog.show();
+                billDaySelector.show();
 
-                WindowManager.LayoutParams params = listDialog.getWindow().getAttributes();
-                params.width = 900;
-                params.height = 1500;
-                listDialog.getWindow().setAttributes(params);
+                WindowManager.LayoutParams params1 = billDaySelector.getWindow().getAttributes();
+                params1.width = 900;
+                params1.height = 1500;
+                billDaySelector.getWindow().setAttributes(params1);
+                break;
+
+            case R.id.tv_deadLine:
+                AlertDialog deadLineSelector = new AlertDialog.Builder(this)
+                        .setTitle("请选择每月最后还款日")
+                        .setItems(temp, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                deadLine = which + 1;
+                                tvDeadLine.setText("每月" + deadLine + "日");
+                            }
+                        }).create();
+
+
+                deadLineSelector.show();
+
+                WindowManager.LayoutParams params2 = deadLineSelector.getWindow().getAttributes();
+                params2.width = 900;
+                params2.height = 1500;
+                deadLineSelector.getWindow().setAttributes(params2);
                 break;
 
             case R.id.btn_calculate:
-
-//                Calendar cal = Calendar.getInstance();
-//                cal.clear();
-//                String today = etToday.getText().toString();
-//                cal.set(Integer.parseInt(today.substring(0, 4)),
-//                        Integer.parseInt(today.substring(4, 6)) - 1,
-//                        Integer.parseInt(today.substring(6, 8)));
-//
-//                Calculator.countDaysFromPaymentToDeadLine(Integer.parseInt(etBillDay.getText().toString()),
-//                        Integer.parseInt(etDeadLine.getText().toString()),
-//                        cal);
-
+                Intent intent = new Intent(this, CreditCashResultActivity.class);
+                intent.putExtra("amount", etAmount.getText().toString());
+                intent.putExtra("feeRate", etFee.getText().toString());
+                intent.putExtra("lieYear", lieYear);
+                intent.putExtra("lieMonth", lieMonth);
+                intent.putExtra("lieDay", lieDay);
+                intent.putExtra("billDay", billDay);
+                intent.putExtra("deadLine", deadLine);
+                startActivity(intent);
                 break;
         }
     }
--- app/src/main/java/cn/wangj/calcpro/business/credit/CreditCashResultActivity.java
+++ app/src/main/java/cn/wangj/calcpro/business/credit/CreditCashResultActivity.java
@@ -2,9 +2,15 @@ package cn.wangj.calcpro.business.credit;
 
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Calendar;
+
 import butterknife.BindView;
 import cn.wangj.calcpro.BaseActivity;
 import cn.wangj.calcpro.R;
+import cn.wangj.calcpro.util.Calculator;
+import cn.wangj.calcpro.util.Logger;
 
 public class CreditCashResultActivity extends BaseActivity {
 
@@ -23,6 +29,10 @@ public class CreditCashResultActivity extends BaseActivity {
     @BindView(R.id.tv_interest)
     TextView tvTrueInterest;
 
+    private double amount, feeRate;
+    private int lieYear, lieMonth, lieDay; // 记录选择的刷卡日期
+    private int billDay, deadLine; // 账单日、最后还款日
+
     @Override
     protected int setContentResID() {
         return R.layout.activity_credit_cash_result;
@@ -30,12 +40,53 @@ public class CreditCashResultActivity extends BaseActivity {
 
     @Override
     protected void runOnCreate() {
+        amount = Double.valueOf(getIntent().getStringExtra("amount"));
+        feeRate = Double.valueOf(getIntent().getStringExtra("feeRate"));
+        lieYear = getIntent().getIntExtra("lieYear", 0);
+        lieMonth = getIntent().getIntExtra("lieMonth", 0);
+        lieDay = getIntent().getIntExtra("lieDay", 0);
+        billDay = getIntent().getIntExtra("billDay", 0);
+        deadLine = getIntent().getIntExtra("deadLine", 0);
+
+        // 计算手续费
+        BigDecimal feeBigD = new BigDecimal(amount)
+                .multiply(new BigDecimal(feeRate))
+                .divide(new BigDecimal(100), 2, RoundingMode.HALF_UP);
+        double fee = feeBigD.doubleValue();
+        tvFee.setText(String.valueOf(fee));
+
+        // 计算到手金额
+        double result = new BigDecimal(amount).subtract(feeBigD).setScale(2, RoundingMode.HALF_UP).doubleValue();
+        tvResult.setText(String.valueOf(result));
+
+        // 计算免息天数
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.set(lieYear, lieMonth, lieDay);
+        int days = Calculator.countDaysFromPaymentToDeadLine(billDay, deadLine, calendar);
+        tvDays.setText(String.valueOf(days));
+
+        // 计算用款成本
+        BigDecimal costBigD = new BigDecimal(feeRate)
+                .multiply(new BigDecimal(365))
+                .divide(new BigDecimal(days), 2, RoundingMode.HALF_UP);
+
+        tvTrueInterest.setText(String.format("%s%%", costBigD.doubleValue()));
+
+        Logger.d(TAG, "???");
+
 
     }
 
     @Override
     protected void runOnResume() {
 
+
+    }
+
+    private double x(double amount, double feeRate){
+
+        return 0;
     }
 
 }
