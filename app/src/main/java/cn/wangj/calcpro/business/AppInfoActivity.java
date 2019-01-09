090238ec4e88399d85b91c431d4b44de19ecc131
WorkComputer_WangJ <jie581825@yeah.net>
2019-01-09 15:11:55 +0800

    代码暂存

---
+++ app/src/main/java/cn/wangj/calcpro/business/AppInfoActivity.java
@@ -0,0 +1,22 @@
+package cn.wangj.calcpro.business;
+
+import cn.wangj.calcpro.BaseActivity;
+import cn.wangj.calcpro.R;
+
+public class AppInfoActivity extends BaseActivity {
+
+    @Override
+    protected int setContentResID() {
+        return R.layout.activity_app_info;
+    }
+
+    @Override
+    protected void runOnCreate() {
+
+    }
+
+    @Override
+    protected void runOnResume() {
+
+    }
+}
--- app/src/main/java/cn/wangj/calcpro/business/MainActivity.java
+++ app/src/main/java/cn/wangj/calcpro/business/MainActivity.java
@@ -11,8 +11,6 @@ import cn.wangj.calcpro.business.credit.CreditCashActivity;
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
 
-//    TextToSpeech textToSpeech;
-
     @Override
     protected int setContentResID() {
         return R.layout.activity_main;
@@ -20,18 +18,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void runOnCreate() {
-//        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-//            @Override
-//            public void onInit(int status) {
-//                if (status == TextToSpeech.SUCCESS) {
-//                    int result = textToSpeech.setLanguage(Locale.CHINA);
-//                    if (result == TextToSpeech.LANG_MISSING_DATA
-//                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-//                        Toast.makeText(MainActivity.this, "暂不支持中文",Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            }
-//        });
     }
 
     @Override
@@ -42,14 +28,15 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @OnClick(R.id.btn_creditLie)
     @Override
     public void onClick(View v) {
+        Intent intent;
         switch (v.getId()) {
             case R.id.btn_creditLie:
-                Intent intent = new Intent(this, CreditCashActivity.class);
+                intent = new Intent(this, CreditCashActivity.class);
                 startActivity(intent);
+                break;
 
-//                String text = "易收宝到账105.50元";
-//                textToSpeech.setPitch(1.5f); // 在系统设置里也可以修改音调
-//                textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+            case R.id.btn_trivia:
+                intent = null;
                 break;
         }
     }
